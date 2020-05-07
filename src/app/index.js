0e2a4ff0dce0a5d5df59e1dc42195b004e05f060
irfangg <irfangg66@gmail.com>
2020-05-07 22:56:04 +0530

    implemnted sorting

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,19 @@
 import "./style.scss";
 
-function fetchAllProducts() {
+function fetchAllProducts(sortType) {
   fetch("https://my-json-server.typicode.com/irfangg/my-apparal-api/apparals")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      if (sortType === "lowToHigh") {
+        data.sort((a, b) => a.Final_Price - b.Final_Price);
+      }
+      if (sortType === "highToLow") {
+        data.sort((a, b) => b.Final_Price - a.Final_Price);
+      }
+      if (sortType === "discount") {
+        data.sort((a, b) => a.Discount_Percent - b.Discount_Percent);
+      }
       data.forEach((d) => {
         $(".products").append(`
           <div class="card product-item">
@@ -16,14 +25,20 @@ function fetchAllProducts() {
               <div class="card-block">
               <div class="card-title">${d.product_type_name}</div>
               <div class="price-details">
-              <span class="finalprice">${d.Final_Price.toFixed(2)}</span>
+              <span class="finalprice">
+              <i class='fa fa-rupee'>${d.Final_Price.toFixed(2)}</i>
+              </span>
               <span class="originalprice">${d.Original_Price.toFixed(2)}</span>
               <span class="discountpercentage">${d.Discount_Percent}%</span>
               </div>
               <a
                   href="#"
+                  data-id=${d.id}
                   data-name="${d.title}"
-                  data-price=${d.Final_Price}
+                  data-price=${d.Original_Price}
+                  data-image=${d.medium_image_url}
+                  data-finalprice=${d.Final_Price}
+                  data-disc=${d.Discount_Percent}
                   class="add-to-cart btn btn-primary"
                   >Add to cart</a
               >
@@ -34,9 +49,14 @@ function fetchAllProducts() {
 
       $(".product-item a.add-to-cart").on("click", function (event) {
         event.preventDefault();
+        var id = $(this).data("id");
+        var image = $(this).data("image");
         var name = $(this).data("name");
-        var price = Number($(this).data("price"));
-        shoppingCart.addItemToCart(name, price, 1);
+        var finalPrice = $(this).data("finalprice");
+        var price = $(this).data("price");
+        var disc = $(this).data("disc");
+
+        shoppingCart.addItemToCart(id, image, name, finalPrice, price, disc, 1);
         displayCart();
       });
     });
@@ -47,7 +67,7 @@ function showProducts(minPrice, maxPrice) {
     .hide()
     .filter(function () {
       console.log(this, minPrice, maxPrice);
-      var price = parseInt($(this).find(" .finalprice").html(), 10);
+      var price = parseInt($(this).find(" .finalprice i.fa-rupee").html(), 10);
       return price >= minPrice && price <= maxPrice;
     })
     .show();
@@ -70,7 +90,7 @@ $(function () {
 
         $(ui.handle)
           .find("span")
-          .html("$" + ui.value);
+          .html(`<i class='fa fa-rupee'>${ui.value}</i>`);
         showProducts(min, max);
       },
     },
@@ -83,7 +103,9 @@ $(function () {
   max = $("#slider-range").slider("values", 1);
 
   $("#max")
-    .html("$" + $("#slider-range").slider("values", 1))
+    .html(
+      `<i class='fa fa-rupee'>${$("#slider-range").slider("values", 1)}</i>`
+    )
     .position({
       my: "center top",
       at: "center bottom",
@@ -92,7 +114,9 @@ $(function () {
     });
 
   $("#min")
-    .html("$" + $("#slider-range").slider("values", 0))
+    .html(
+      `<i class='fa fa-rupee'>${$("#slider-range").slider("values", 0)}</i>`
+    )
     .position({
       my: "center top",
       at: "center bottom",
@@ -103,6 +127,15 @@ $(function () {
   showProducts(min, max);
 
   fetchAllProducts();
+
+  $(".sortBy ul > li > a").click(function (e) {
+    e.preventDefault();
+    $("ul > li > a").addClass("notActiveSort");
+    $(this).removeClass("notActiveSort");
+    $(".products").empty();
+    let sortBy = $(this).data("value");
+    fetchAllProducts(sortBy);
+  });
 });
 
 var shoppingCart = (function () {
@@ -112,9 +145,13 @@ var shoppingCart = (function () {
   let cart = [];
 
   // Constructor
-  function Item(name, price, count) {
+  function Item(id, image, name, finalPrice, price, disc, count) {
+    this.id = id;
+    this.image = image;
     this.name = name;
+    this.finalPrice = finalPrice;
     this.price = price;
+    this.disc = disc;
     this.count = count;
   }
 
@@ -137,31 +174,40 @@ var shoppingCart = (function () {
   var obj = {};
 
   // Add to cart
-  obj.addItemToCart = function (name, price, count) {
+  obj.addItemToCart = function (
+    id,
+    image,
+    name,
+    finalPrice,
+    price,
+    disc,
+    count
+  ) {
     for (var item in cart) {
-      if (cart[item].name === name) {
+      if (cart[item].id === id) {
         cart[item].count++;
         saveCart();
         return;
       }
     }
-    var item = new Item(name, price, count);
+    console.log("on add cart ", cart);
+    var item = new Item(id, image, name, finalPrice, price, disc, count);
     cart.push(item);
     saveCart();
   };
   // Set count from item
-  obj.setCountForItem = function (name, count) {
+  obj.setCountForItem = function (id, count) {
     for (var i in cart) {
-      if (cart[i].name === name) {
+      if (cart[i].id === id) {
         cart[i].count = count;
         break;
       }
     }
   };
   // Remove item from cart
-  obj.removeItemFromCart = function (name) {
+  obj.removeItemFromCart = function (id) {
     for (var item in cart) {
-      if (cart[item].name === name) {
+      if (cart[item].id === id) {
         cart[item].count--;
         if (cart[item].count === 0) {
           cart.splice(item, 1);
@@ -173,9 +219,9 @@ var shoppingCart = (function () {
   };
 
   // Remove all items from cart
-  obj.removeItemFromCartAll = function (name) {
+  obj.removeItemFromCartAll = function (id) {
     for (var item in cart) {
-      if (cart[item].name === name) {
+      if (cart[item].id === id) {
         cart.splice(item, 1);
         break;
       }
@@ -249,35 +295,35 @@ $(".clear-cart").click(function () {
 
 function displayCart() {
   var cartArray = shoppingCart.listCart();
+  console.log("cartArray ", cartArray);
   var output = "";
-  for (var i in cartArray) {
-    output +=
-      "<tr>" +
-      "<td>" +
-      cartArray[i].name +
-      "</td>" +
-      "<td>(" +
-      cartArray[i].price +
-      ")</td>" +
-      "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" +
-      cartArray[i].name +
-      ">-</button>" +
-      "<input type='number' class='item-count form-control' data-name='" +
-      cartArray[i].name +
-      "' value='" +
-      cartArray[i].count +
-      "'>" +
-      "<button class='plus-item btn btn-primary input-group-addon' data-name=" +
-      cartArray[i].name +
-      ">+</button></div></td>" +
-      "<td><button class='delete-item btn btn-danger' data-name=" +
-      cartArray[i].name +
-      ">X</button></td>" +
-      " = " +
-      "<td>" +
-      cartArray[i].total +
-      "</td>" +
-      "</tr>";
+  for (let item of cartArray) {
+    output += `
+    <div class="cart-item">
+      <img src=${item.image} alt=${item.name} />
+      <div class="cart-item-details">
+        <h6>${item.name}</h6>
+        <div class="price-details">
+          <span class="finalprice">${item.finalPrice?.toFixed(2)}</span>
+          <span class="originalprice">${item.price?.toFixed(2)}</span>
+          <span class="discountpercentage">${item.disc}%</span>
+        </div>
+      </div>
+      <div class='input-group cart-item-count'>
+        <i class="fa fa-2x fa-minus-circle minus-item input-group-addon btn btn-primary"
+          data-id="${item.id}"></i>
+        <input type='number' class='item-count form-control' 
+          data-id="${item.id}" 
+          value=${item.count}>
+        <i class="fa fa-2x fa-plus-circle plus-item btn btn-primary input-group-addon"
+          data-id="${item.id}"></i>
+      </div>
+      <div class="cart-item-delete">
+      <button class='delete-item btn btn-danger' 
+        data-id="${item.id}">Remove</button>
+      </div>
+    </div>
+    `;
   }
   $(".show-cart").html(output);
   $(".total-cart").html(shoppingCart.totalCart());
@@ -287,34 +333,30 @@ function displayCart() {
 // Delete item button
 
 $(".show-cart").on("click", ".delete-item", function (event) {
-  var name = $(this).data("name");
-  shoppingCart.removeItemFromCartAll(name);
+  var id = $(this).data("id");
+  shoppingCart.removeItemFromCartAll(id);
   displayCart();
 });
 
 // -1
 $(".show-cart").on("click", ".minus-item", function (event) {
-  var name = $(this).data("name");
-  shoppingCart.removeItemFromCart(name);
+  var id = $(this).data("id");
+  shoppingCart.removeItemFromCart(id);
   displayCart();
 });
 // +1
 $(".show-cart").on("click", ".plus-item", function (event) {
-  var name = $(this).data("name");
-  shoppingCart.addItemToCart(name);
+  var id = $(this).data("id");
+  shoppingCart.addItemToCart(id);
   displayCart();
 });
 
 // Item count input
 $(".show-cart").on("change", ".item-count", function (event) {
-  var name = $(this).data("name");
+  var id = $(this).data("id");
   var count = Number($(this).val());
-  shoppingCart.setCountForItem(name, count);
+  shoppingCart.setCountForItem(id, count);
   displayCart();
 });
 
 displayCart();
-
-$(".cart-items").on("click", function () {
-  alert();
-});
